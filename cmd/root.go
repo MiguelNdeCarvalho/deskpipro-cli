59c715e4503e68c43adfd868bb7a74d96029e23c
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-02-13 15:46:49 +0000

    Add Base

---
+++ cmd/root.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "deskpi-cli",
+	Short: "DeskPi-CLI is an Unofficial tool to control DeskPi devices",
+	Long:  "DeskPi-CLI is an Unofficial tool to control DeskPi devices",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ deskpi_cli.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/MiguelNdeCarvalho/deskpi-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
